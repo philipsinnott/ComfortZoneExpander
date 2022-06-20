75872dd3b334a44fc06319915848f95e6946963c
Philip Sinnott <philipsinnotts@gmail.com>
2022-06-20 16:02:35 +0200

    Add project files.

---
+++ ComfortZoneExpanderWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComfortZoneExpanderWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
